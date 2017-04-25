e39d19224fcf506ec77f15c8b41a0538c470467b
Tian Zhao <tianzhao@tucson.Stanford.EDU>
2017-04-25 14:33:58 -0700

    added dev branch for publishing

---
+++ src/ArgInOut.scala
@@ -0,0 +1,25 @@
+import org.virtualized._
+import spatial._
+
+object ArgInOut extends SpatialApp {
+  import IR._
+
+  @virtualize
+  def main() {
+    val x = ArgIn[Int]
+    val y = ArgOut[Int]
+    val N = args(0).to[Int]
+
+    setArg(x, N)
+
+    Accel {
+      // TODO: Set y = x + 4 here
+      y := x + 4
+    }
+
+    val result = getArg(y)
+    val gold = N + 4
+    println("expected: " + gold)
+    println("result: " + result)
+  }
+}
